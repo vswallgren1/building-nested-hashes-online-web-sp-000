920417018779daa5c17ca43f4b44d938c3c975c5
Vince W <vincewallgren@me.com>
2021-02-27 05:22:29 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,8 +20,9 @@ def bonus
    }
   }
 
-    epic_tragedy[:montague][:hero][:status] = "dead"
-    epic_tragedy[:capulet][:heroine][:status] = "dead"
+  epic_tragedy[:montague][:hero][:status] = "dead"
+
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
 
 
 
